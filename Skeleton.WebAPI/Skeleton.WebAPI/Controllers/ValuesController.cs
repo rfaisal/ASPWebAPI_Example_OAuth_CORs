ccff388e7ea8da2671ef444033c82392fa473aff
Faisal Rahman <md.faisal.rahman@gmail.com>
2014-01-12 18:21:51 -0500

    VS project added, ValuesController modified

---
+++ Skeleton.WebAPI/Skeleton.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Skeleton.WebAPI.Controllers
+{
+   // [Authorize]
+    public class ValuesController : ApiController
+    {
+        private static List<string> values = new List<string>();
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return values;
+        }
+
+        // GET api/values/5
+        public string Get(int index)
+        {
+            return values.ElementAtOrDefault(index);
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            values.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int index, [FromBody]string value)
+        {
+            if (values.Count > index)
+                values[index] = value;
+            else
+                Post(value);
+        }
+
+        // DELETE api/values/5
+        public void Delete(int index)
+        {
+            if (values.Count > index)
+                values.RemoveAt(index);
+        }
+    }
+}
---
+++ Skeleton.WebAPI/Skeleton.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Skeleton.WebAPI.WebApiApplication" Language="C#" %>
