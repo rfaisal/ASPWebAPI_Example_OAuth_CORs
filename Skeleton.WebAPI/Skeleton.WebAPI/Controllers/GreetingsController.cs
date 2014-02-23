4861e43088c8570c92756ae64cb986d83e6f67f8
Faisal Rahman <md.faisal.rahman@gmail.com>
2014-02-23 15:04:03 -0500

    Greetings controller added

---
+++ Skeleton.WebAPI/Skeleton.WebAPI/Controllers/GreetingsController.cs
@@ -0,0 +1,58 @@
+﻿using Skeleton.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Skeleton.WebAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+   // [Authorize]
+    public class GreetingsController : ApiController
+    {
+        private static List<Greeting> values = new List<Greeting>();
+        // GET api/values
+        public IEnumerable<Greeting> Get()
+        {
+            return values;
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            int max = 0;
+            for (int i = 0; i < values.Count; i++)
+                if (values[i].id > max)
+                    max = values[i].id;
+            values.Add(new Greeting { id = max+1, content = value});
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            for (int i = 0; i < values.Count; i++)
+            {
+                if (values[i].id == id)
+                {
+                    values[i].content = value;
+                    break;
+                }
+            }
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            for (int i = 0; i < values.Count; i++)
+            {
+                if (values[i].id == id)
+                {
+                    values.RemoveAt(i);
+                }
+            }   
+        }
+    }
+}
---
+++ Skeleton.WebAPI/Skeleton.WebAPI/Models/Greeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Skeleton.WebAPI.Models
+{
+    public class Greeting
+    {
+        public int id { get; set; }
+        public string content { get; set; }
+    }
+}
