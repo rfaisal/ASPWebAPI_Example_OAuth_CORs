5c65ec7b7f6a386179b602bcf8de785bc6879c81
Faisal Rahman <md.faisal.rahman@gmail.com>
2014-02-23 15:18:58 -0500

    JSON response

--- Skeleton.WebAPI/Skeleton.WebAPI/App_Start/WebApiConfig.cs
+++ Skeleton.WebAPI/Skeleton.WebAPI/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
 
 namespace Skeleton.WebAPI
 {
@@ -26,6 +27,7 @@ namespace Skeleton.WebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
