77ea439dbabc69effd208b481f8a58bc3c010b07
Faisal Rahman <md.faisal.rahman@gmail.com>
2014-04-23 08:49:13 -0400

    php client added

---
+++ temp/test.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "This line makes sure this is PHP";
+?>
+<input type="button" value="Click Me" onclick="window.location='http://localhost:49811/api/Account/ExternalLogin?provider=Facebook&response_type=token&client_id=self&redirect_uri=http%3A%2F%2Flocalhost%3A49811%2F&state=zwm1905DiChZHzuVqksZAB6KLbY9h6Lb4swmLl_2xFo1'" />
+
+</body>
+</html>
